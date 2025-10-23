236103798410821494cc029876ee6f592aaaabda
PacoRex <acamicamacho@gmail.com>
2025-10-23 08:38:22 -0500

    Iniciando en el desarrollo con Python

---
+++ DÍA 1/Ejercicios práctica print/ejercicio_2_print.py
@@ -0,0 +1,12 @@
+'''
+
+Práctica Print 2
+Crea un código que imprima en pantalla la expresión Estudiar con "Python Total" es super divertido
+
+Aclaración: presta mucha atención al texto que debes reproducir, ya que errores ortográficos y de
+puntuación, espacios en blanco adicionales y saltos de línea innecesarios pueden ocasionar errores
+inesperados que no son detectados por el módulo de evaluación, devolviendo un error en tu ejercicio.
+
+'''
+
+print('Estudiar con "Python Total" es super divertido')
---
+++ DÍA 1/Ejercicios prácticos input/ejercicio_2_input.py
@@ -0,0 +1,19 @@
+'''
+
+Práctica Input 2
+Crea un código que le permita ingresar una respuesta al usuario, haciéndole la siguiente pregunta:
+
+¿En qué país vives?
+
+El código debe poder imprimir en pantalla lo ingresado por el usuario (utilizando print).
+
+Aclaración:
+
+1. no deben imprimirse strings adicionales a la respuesta del usuario.
+
+2. Los ejercicios que contienen el la función input() deben ser probados con el botón: "Ejecutar pruebas".
+Ya que el botón "Ejecutar código" arrojará el siguiente error: "EOF when reading a line".
+
+'''
+
+print(input("¿En qué país vives?"))
